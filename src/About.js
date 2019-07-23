f4bc49dd1180c2dc9ae83d21da2b9cb138497a43
Yong Kai Yi <y.kaiyi@outlook.com>
2019-07-23 13:22:38 +0800

    Update About.js

--- src/About.js
+++ src/About.js
@@ -10,7 +10,7 @@ class About extends Component {
 					Not reddit, for cats.
 				</div>
 				<div className="body">
-					<p>Hi Carousell! Thanks for considering me as a candidate. This is my first time using javascript (and React). It was fun.</p>
+					<p>This was built over a weekend for a take-home interview assignment. This is my first time using javascript (and React). It was fun.</p>
 					<p>This simple app was compiled using create-react-app to generate the skeleton code. The pages were written by myself.</p>
 					<p>The app loads a few dummy topics when started, these can be found in the dummyTopics.js file in /src.</p>
 					<p>Hope you like it and thanks again!</p>
